65180edc2d25571af74c971ec57835ea2fb328f0
Ezequiel Trindade <ezequielrt.dev@gmail.com>
2024-08-26 08:28:13 -0300

    Add project files.

---
+++ Dima.Core/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿namespace Dima.Core.Models;
+
+public class Transaction
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+    public DateTime? PaidOrReceveidAt { get; set; }
+
+    public int Type { get; set; }
+    public decimal Amount { get; set; }
+
+    public long CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+
+    public string UserId { get; set; } = string.Empty;
+}
